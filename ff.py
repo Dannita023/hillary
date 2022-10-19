5cd8e139c96bd3fa14830510880a7ca44a7fe03f
B14-AINF <B14@academico.unab.ad>
2022-10-19 11:50:52 -0500

    first commit

---
+++ ff.py
@@ -0,0 +1,14 @@
+precio = 600
+
+item = "chocolatina"
+
+print("La %s cuesta %d pesos"%(item,precio))
+
+print("La %s cuesta %5.2f pesos"%(item,precio))
+
+print("La %s cuesta %10.2f pesos"%(item,precio))
+
+diccitem = {"item":"chocolatina","costo":600}
+
+print("La %(item)s cuesta %(costo)7.1f pesos"%diccitem)
+
